ae64ac15226ff73e25b20c9d62dcea5bf6e65682
Amaiv Simau <iramaro@mail.com>
2015-09-29 15:44:44 -0300

    Exercício 3 da lista 1 concluído.

---
+++ Listas/Lista_1/Ex_3/Ex_3.java
@@ -0,0 +1,18 @@
+public class Ex_3
+{
+	public static void main(String[] args)
+	{	
+		float comodo_1, comodo_2, comodo_3, total_area;
+		comodo_1 = 100;
+		comodo_2 = 35;
+		comodo_3 = 35;
+		total_area = 0;
+		total_area = comodo_1 + comodo_2 + comodo_3;
+		
+		System.out.println("++++++++ Tamanho da sua casa ++++++++\n");
+		System.out.println("\nSala: "+comodo_1+"m²\n");
+		System.out.println("\nQuarto: "+comodo_2+"m²\n");
+		System.out.println("\nBanheiro: "+comodo_3+"m²\n");
+		System.out.println("\nTotal: "+total_area+"m²\n");
+	}
+}	
---
+++ Listas/Lista_1/Ex_3/README.md
@@ -0,0 +1,11 @@
+## Exercício 3
+
+Execute o seguinte programa e escreva exatamente qual será a saída:
+
+Saída:
+
+Tamanho da sua casa:
+Sala: 100m²
+Quarto: 35m²
+Banheiro: 35m²
+Total: 170m²
